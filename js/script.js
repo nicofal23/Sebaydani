07ec8b96dd0ac167c7adc9dd2d6c1d47df510943
Nicolas Falciglio <136000929+nicofal23@users.noreply.github.com>
2025-03-18 21:34:29 -0300

    lugar

--- js/script.js
+++ js/script.js
@@ -110,3 +110,26 @@ document.addEventListener("DOMContentLoaded", () => {
         isDragging = false;
     });
 });
+
+
+
+
+//google maps 
+
+// Función para abrir el modal
+function openModal() {
+    document.getElementById("mapsModal").style.display = "block";
+}
+
+// Función para cerrar el modal
+function closeModal() {
+    document.getElementById("mapsModal").style.display = "none";
+}
+
+// Cerrar el modal si se hace clic fuera del contenido
+window.onclick = function (event) {
+    const modal = document.getElementById("mapsModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
